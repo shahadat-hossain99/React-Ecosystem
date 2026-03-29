420bfbb48c58b9108ff510fe362e58c854e74704
shahadat-hossain99 <shahadathossain31500@gmail.com>
2026-03-29 11:53:47 +0600

    pricingCard redo with the help od daisyUI

--- src/Components/PricingCard/PricingCard.jsx
+++ src/Components/PricingCard/PricingCard.jsx
@@ -5,20 +5,23 @@ const PricingCard = ({ pricing }) => {
   console.log(pricing);
   const { name, price, description, features } = pricing;
   return (
-    <div className="border bg-sky-500 rounded-2xl p-5 mx-4">
+    <div className="border bg-sky-500 rounded-2xl p-5 mx-4 mb-4 flex flex-col">
       {/* Card Header */}
       <div>
         <h1 className="text-7xl">{name}</h1>
         <h4 className="text-3xl">{price}</h4>
       </div>
       {/* Card body */}
-      <div className="bg-sky-300 rounded-2xl p-4 mt-8">
+      <div className="bg-sky-300 rounded-2xl p-4 mt-8 flex-1">
         <p>{description}</p>
 
         {features.map((features, index) => (
           <PricingFeature key={index} features={features}></PricingFeature>
         ))}
       </div>
+      <button className="btn rounded-xl btn-soft btn-info w-full mt-5">
+        Subscribe Now !!!
+      </button>
     </div>
   );
 };
--- src/Components/PricingOptions/PricingOptions.jsx
+++ src/Components/PricingOptions/PricingOptions.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { use } from "react";
 import PricingCard from "../PricingCard/PricingCard";
+import DaisyPricing from "../DaisyPricing/DaisyPricing";
 
 const PricingOptions = ({ pricingPromise }) => {
   //   console.log(pricingPromise);
@@ -16,9 +17,14 @@ const PricingOptions = ({ pricingPromise }) => {
     <div>
       <h1 className="text-5xl text-center my-16">Get Our MemberShip</h1>
 
-      <div className="grid   md:grid-cols-3 gap-8">
-        {pricingData.map((pricing) => (
+      <div className="grid md:grid-cols-2  lg:grid-cols-3 gap-8">
+        {/* Card From scratch by me */}
+        {/* {pricingData.map((pricing) => (
           <PricingCard key={pricing.id} pricing={pricing}></PricingCard>
+        ))} */}
+
+        {pricingData.map((pricing) => (
+          <DaisyPricing key={pricing.id} pricing={pricing}></DaisyPricing>
         ))}
       </div>
     </div>
